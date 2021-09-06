f28253d99578b5c54b91b5e6db9374121e0d94e9
Mrinal Seth <mrinalseth@pop-os.localdomain>
2021-09-06 13:02:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Input, FormControl, FormLabel } from '@material-ui/core'
+import React, {useEffect, useState} from 'react'
+import Message from './components/Message'
+import db from './firebase' 
+import firebase from 'firebase/compat'
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [messages, setMessages] = useState([])
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    setUsername(prompt('enter ur username'))
+  }, [])
+  useEffect(() => {
+    db.collection('messages')
+    .orderBy('timestamp', 'asc')
+    .onSnapshot((snap) => {
+      setMessages(snap.docs.map((doc) => {
+          return (doc.data())
+      }))
+    })
+  }, [])
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    db.collection('messages').add({
+      text: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setInput('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello there</h1>
+      <form>
+        <FormControl>
+          <FormLabel>Enter a message</FormLabel>
+          <Input
+          value = {input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button disabled={!input} type='submit' onClick={sendMessage} variant='outlined' color='primary'>Send!</Button>
+        </FormControl>
+      </form>
+      {messages.map((msg) => {
+        return (
+          <Message
+            username={username}
+            message={msg}
+          />
+        )
+      })}
     </div>
   );
 }
